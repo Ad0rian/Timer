faf093a5762a70357a3a45b72ace07bb299a3c3d
Ad0rian <justisatroubleday@gmail.com>
2021-10-01 19:33:12 +0200

    Initial commit

---
+++ SimpletimerNonEnding.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpletimerNonEnding : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent m_MyEvent;
+    public float targetTime;
+
+    private float counterTime;
+
+    private void Start()
+   {
+      counterTime= targetTime;
+   }
+
+    void Update(){
+ 
+   counterTime -= Time.deltaTime;
+   
+   if (counterTime <= 0.0f)
+      {
+         timerEnded();
+      }
+   }
+   
+   void timerEnded()
+   {
+      m_MyEvent.Invoke();
+      counterTime= targetTime;
+   }
+}
